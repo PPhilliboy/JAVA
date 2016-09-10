ff7d2dbc93573ac053ef4ee558b7ee6038013571
the1stDevice <gerrit.buecken@mailbox.tu-dresden.de>
2016-09-10 14:03:35 +0200

    Add files via upload

---
+++ Signal.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse Signal.
+ * 
+ * @author (Ihr Name) 
+ * @version (eine Versionsnummer oder ein Datum)
+ */
+public class Signal
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse Signal
+     */
+    public Signal()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
