e666347f0b84299fe531aa48614bf0a039fc46b0
the1stDevice <gerrit.buecken@mailbox.tu-dresden.de>
2016-10-05 16:55:00 +0200

    new NAND

--- Nand.java
+++ Nand.java
@@ -1,33 +1,53 @@
 
 /**
- * Beschreiben Sie hier die Klasse Nand.
+ *  Die Klasse NAND beinhaltet die Berechnungsvorschrift einens NAND-Gatters mit variabler Eingangsanzahl.
  * 
  * @author (Ihr Name) 
- * @version (eine Versionsnummer oder ein Datum)
+ * @version 05.10.2016
  */
 public class Nand
 {
-    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
-    private int x;
-
+    private Signal[] entrances;     /** die Eingänge des Gatters vom Typ Signal */
+    private Signal exit;            /** der Ausgabg des Gatters */
     /**
      * Konstruktor für Objekte der Klasse Nand
      */
-    public Nand()
+    public Nand(int numbEntrance)
     {
-        // Instanzvariable initialisieren
-        x = 0;
+        entrances = new Signal [numbEntrance];
     }
 
     /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
+     * die Methode setInput verknüpft die Gatter untereinander und weist den Eingängen Signale zu
+     * das Gatter weist sich selbst als Ziel eines anderen Gatters zu
      */
-    public int beispielMethode(int y)
+    public void setInput (int entrance,Signal signal)
     {
-        // tragen Sie hier den Code ein
-        return x + y;
+        entrances[entrance] = signal;
+        signal.setTarget(this);
+    }
+
+    /**
+     *  die Methode setOutput weist das Ausgangssignal zu
+     */
+    public void setOutput (Signal signal)
+    {
+        exit = signal;
+    }
+
+    /**
+     * calculate berechnet den Wert des AusgangsSignals mittels logischer Verknüpfung der Eingänge
+     * der erste Eingang wird mit dem zweiten UND-Verknüpft, das Ergebnis wiederum mit dem nächsten Eingang etc.
+     * wichtig ist hierbei das vorherige Setzen des Ergebnisses auf True
+     */
+    public void calculate()
+    {
+        boolean result = true;
+        for( int i = 0; i<entrances.length; i++)
+        {
+            result = result & entrances[i].getValue();
+        }
+        result = !result;
+        exit.setValue(result);
     }
 }
