c0a8ee238c4948c99b3bd79f05396894e79b5b5c
the1stDevice <gerrit.buecken@mailbox.tu-dresden.de>
2017-09-12 14:13:53 +0200

    Add files via upload
    added needed .java files to directory

--- Nand.java
+++ Nand.java
@@ -1,25 +1,32 @@
 
 /**
- *  Die Klasse NAND beinhaltet die Berechnungsvorschrift einens NAND-Gatters mit variabler Eingangsanzahl.
- * 
- * @author (Ihr Name) 
- * @version 05.10.2016
+ *  Die Klasse NAND beinhaltet die Berechnungsvorschrift einens NAND-Gatters
+ *  mit variabler Eingangsanzahl.
+ * es gibt zwei gespeicherte Variablen:
+ *  - entrances: ein Array, welches Objekte vom Typ Signal beinhaltet;
+ *    die Länge gibt die Anzahl der Eingänge an
+ *  - exit: Typ Signal; ist das Ausgangssignal
+ * @author (Phillip Schneevoigt und Gerrit Bücken) 
+ * @version 12.09.2017
  */
 public class Nand
 {
-    private Signal[] entrances;     /** die Eingänge des Gatters vom Typ Signal */
-    private Signal exit;            /** der Ausgabg des Gatters */
+    private Signal[] entrances;    
+    private Signal exit;            
     /**
      * Konstruktor für Objekte der Klasse Nand
+     * es wird eine Anzahl an Eingängen übergeben
+     * es wird generell von einem Ausgang ausgegangen
      */
-    public Nand(int numbEntrance)
+    public Nand(int number_of_entrance)
     {
-        entrances = new Signal [numbEntrance];
+        entrances = new Signal [number_of_entrance];
     }
 
     /**
-     * die Methode setInput verknüpft die Gatter untereinander und weist den Eingängen Signale zu
-     * das Gatter weist sich selbst als Ziel eines anderen Gatters zu
+     * die Methode setInput verknüpft die Gatter untereinander und weist den
+     * Eingängen Signale zu das Gatter weist sich selbst als Ziel eines
+     * anderen Gatters zu
      */
     public void setInput (int entrance,Signal signal)
     {
@@ -36,14 +43,15 @@ public class Nand
     }
 
     /**
-     * calculate berechnet den Wert des AusgangsSignals mittels logischer Verknüpfung der Eingänge
-     * der erste Eingang wird mit dem zweiten UND-Verknüpft, das Ergebnis wiederum mit dem nächsten Eingang etc.
+     * calculate berechnet den Wert des AusgangsSignals mittels logischer
+     * Verknüpfung der Eingänge der erste Eingang wird mit dem zweiten
+     * UND-Verknüpft, das Ergebnis wiederum mit dem nächsten Eingang etc.
      * wichtig ist hierbei das vorherige Setzen des Ergebnisses auf True
      */
     public void calculate()
     {
         boolean result = true;
-        for( int i = 0; i<entrances.length; i++)
+        for( int i = 0; i < entrances.length; i++)
         {
             result = result & entrances[i].getValue();
         }
--- Signal.java
+++ Signal.java
@@ -2,50 +2,70 @@ import java.util.ArrayList;
 /**
  * Signal beinhaltet die Information über den Wert
  * weiterhin wird ist die Klasse für das Ausdrucken auf die Konsole verantwortlich
+ * gespeicherte Variablen:
+ *                        - name: Typ String; Name des Signals
+ *                        - value: Typ boolean; Wert des Signals
+ *                        - target_list: Typ ArrayList<Nand>; Liste, welche die
+ *                                      Zielanschlüsse beinhaltet
  * @author (Ihr Name) 
  * @version (eine Versionsnummer oder ein Datum)
  */
 public class Signal
 {
-    private String name;
-    private boolean value;
-    private ArrayList<Nand> target;
+    private String name;  // name des Signals, dient der identifikation
+    private boolean value;  // Wert des Signals, kann etweder wahr oder falsch sein
+    private ArrayList<Nand> target_list;  /** Liste, die die Zielanschlüsse des
+                                              nächsetn Gatters enthält
+                                            */
     /**
      * Konstruktor für Objekte der Klasse Signal
      */
     public Signal(String name)
     {
-        this.name = name;
+        this.name = name;  // setzt den Namen des Objekts Signal
         value = false;
-        target = new ArrayList<Nand>();
+        target_list = new ArrayList<Nand>();
     }
 
     /**
-     * setValue setzt den neuen Wert des jeweiligen Signals mittels berechnung und druckt die Ergebnisse des letzten Gatters aus
+     * setValue setzt den neuen Wert des jeweiligen Signals mittels
+     * berechnung und druckt die Ergebnisse des letzten Gatters aus
      */
     public void setValue(boolean value)
     {
-        this.value = value;
-        if (target.size() == 0)
+        this.value = value;  // setzt den Wert des Objekts Signal
+        /**
+         * falls es keine Ziele mehr in der Liste gibt soll die Berechnung beendet
+         * und das Ergebnis ausgedruckt werden
+         */
+        if (target_list.size() == 0)
         {
             System.out.println(name+ " -> "+value);
         }
         else
         {
-            for (int i=0; i<target.size();i++)
+            for (int i=0; i<target_list.size();i++)
             {
-                target.get(i).calculate();
+                target_list.get(i).calculate();
             }
         }
     }
 
     public boolean getValue()
+    /**
+     * gibt den Wert des Signals zurück
+     * eine __getter__ Methode
+     */
     {
         return value;
     }
 
     public void setTarget (Nand gate)
+    /**
+     * fügt ein Ziel (Gatter, hier NAND) hinzu, an welches das berechnete Signal
+     * übergeben werden soll
+     */
     {
-        target.add(gate);
+        target_list.add(gate);
     }
 }
